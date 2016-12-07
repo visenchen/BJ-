f5601e1fbb28d9ec4c40afc413492b30faedd85e
Visen <visenchen@VisendeMacBook-Air.local>
2016-12-07 19:40:42 +0900

    初始化项目

---
+++ BJ直播/BJ直播/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BJ直播
+//
+//  Created by Visen on 2016/12/7.
+//  Copyright © 2016年 Visen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BJ直播/BJ直播Tests/BJ__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BJ__Tests.swift
+//  BJ直播Tests
+//
+//  Created by Visen on 2016/12/7.
+//  Copyright © 2016年 Visen. All rights reserved.
+//
+
+import XCTest
+@testable import BJ__
+
+class BJ__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
